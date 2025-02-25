1bb635bb2e27e8d257a568b81d805d9c93048a8e
Reva Agrawal <ragrawal@ucsd.edu>
2025-02-25 14:51:28 -0800

    map styling

--- map.js
+++ map.js
@@ -2,12 +2,12 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
 // Set Mapbox Access Token
-mapboxgl.accessToken = 'pk.eyJ1IjoicmV2YWFncmF3YWwiLCJhIjoiY203bDIyM2s1MDcyMDJqcHd3OG5zZGl1MCJ9.reoCpN0lmQ-xpy_0vlurrA';
+mapboxgl.accessToken = 'pk.eyJ1IjoicmV2YWFncmF3YWwiLCJhIjoiY203bDJucXRxMDN4YTJsb3AwbTFxdmw4NCJ9.AC78EcgiiILPdc5zEX5rRQ';
 
 // Initialize the map
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/revaagrawal/cm7l2a6g4003k01ss1ke85ge8',
+  style: 'mapbox://styles/mapbox/streets-v12',
   center: [-71.09415, 42.36027], // Boston
   zoom: 12,
   minZoom: 5,
